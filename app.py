c32a872f5b164728db9036f78808179323d234e3
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2025-02-13 19:19:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,35 +14,268 @@ stemmer = stemmer_factory.create_stemmer()
 stopword_factory = StopWordRemoverFactory()
 stopword_remover = stopword_factory.create_stop_word_remover()
 
-# Load normalization dictionary from Excel
-@st.cache_data
-def load_normalization_dict():
-    try:
-        normalized_word = pd.read_excel("kamus perbaikan.xlsx")
-        return {str(row['TIDAK BAKU']).strip(): str(row['BAKU']).strip() 
-                for _, row in normalized_word.iterrows()}
-    except Exception as e:
-        st.error(f"Error loading normalization dictionary: {e}")
-        return {}
-
-
 # Fungsi untuk membersihkan teks
 def clean_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text)  # Menghapus angka dan karakter non-huruf
     return text
 
 # Fungsi Normalisasi
-def normalize_text(text):
-    if pd.isna(text):
-        return text
-    
-    text = str(text)
-    normalized_dict = load_normalization_dict()
-    
-    for term, replacement in normalized_dict.items():
-        pattern = r'\b' + re.escape(term) + r'\b'
-        text = re.sub(pattern, replacement, text, flags=re.IGNORECASE)
-    
+def normalize_negation(text):
+    negation_patterns = {
+       r'\btidak bersih\b': 'kotor',
+        r'\tidak bagus\b': 'jelek',
+        r'\btidak teratur\b': 'berantakan',
+        r'\btidak lengkap\b': 'tidaklengkap',
+        r'\btidak memadai\b': 'kurangmemadai',
+        r'\btidak nyaman\b': 'tidaknyaman',
+        r'\btidak ramah\b': 'tidakramah',
+        r'\btidak segar\b': 'tidaksegar',
+        r'\btidak enak\b': 'tidakenak',
+        r'\btidak sopan\b': 'tidaksopan',
+        r'\btidak profesional\b': 'tidakprofesional',
+        r'\btidak responsif\b': 'cuek',
+        r'\btidak efisien\b': 'tidakefisien',
+        r'\btidak konsisten\b': 'tidakkonsisten',
+        r'\btidak stabil\b': 'tidakstabil',
+        r'\btidak matang\b': 'tidakmatang',
+        r'\btidak membantu\b': 'tidakmembantu',
+        r'\btidak cepat\b': 'lambat',
+        r'\btidak wajar\b': 'aneh',
+        r'\btidak sesuai\b': 'tidaksesuai',
+        r'\btidak aman\b': 'tidakaman',
+        r'\btidak jujur\b': 'tidakjujur',
+        r'\btidak peduli\b': 'cuek',
+        r'\btidak terawat\b': 'tidakterawat',
+        r'\btidak tepat waktu\b': 'tidaktepatwaktu',
+        r'\btidak tanggap\b': 'tidaksigap',
+        r'\btidak bertanggung jawab\b': 'tidakbertanggungjawab',
+        r'\btidak wangi\b': 'bau',
+        r'\btidak layak\b': 'tidaklayak',
+        r'\btidak bisa\b': 'tidakbisa',
+        r'\btidak rapi\b': 'tidakrapi',
+
+        # Kata negasi diawali dengan "kurang"
+        r'\bkurang bersih\b': 'kotor',
+        r'\bkurang bagus\b': 'kotor',
+        r'\bkurang baik\b': 'kurangbaik',
+        r'\bkurang lengkap\b': 'tidaklengkap',
+        r'\bkurang memuaskan\b': 'tidakmemuaskan',
+        r'\bkurang sopan\b': 'tidaksopan',
+        r'\bkurang cepat\b': 'lambat',
+        r'\bkurang nyaman\b': 'tidaknyaman',
+        r'\bkurang ramah\b': 'tidakramah',
+        r'\bkurang segar\b': 'tidaksegar',
+        r'\bkurang profesional\b': 'tidakprofesional',
+        r'\bkurang terawat\b': 'tidakterawat',
+        r'\bkurang efisien\b': 'tidakefisien',
+        r'\bkurang matang\b': 'tidakmatang',
+        r'\bkurang sigap\b': 'tidaksigap',
+        r'\bkurang informatif\b': 'tidakinformatif',
+        r'\bkurang sesuai ekspektasi\b': 'kecewa',
+        r'\bkurang teratur\b': 'berantakan',
+        r'\bkurang konsisten\b': 'tidakkonsisten',
+        r'\bkurang memadai\b': 'kurangmemadai',
+        r'\bkurang responsif\b': 'cuek',
+        r'\bkurang stabil\b': 'tidakstabil',
+        r'\bkurang membantu\b': 'tidakmembantu',
+        r'\bkurang wajar\b': 'aneh',
+        r'\bkurang aman\b': 'tidakaman',
+        r'\bkurang peduli\b': 'cuek',
+        r'\bkurang tepat waktu\b': 'tidaktepatwaktu',
+        r'\bkurang tanggap\b': 'tidaksigap',
+        r'\bkurang wangi\b': 'bau',
+        r'\bkurang layak\b': 'tidaklayak',
+        r'\bkurang enak\b': 'tidakenak',
+        r'\bkurang jujur\b': 'tidakjujur',
+        r'\bkurang bertanggung jawab\b': 'tidakbertanggungjawab',
+        r'\bkurang tepat\b': 'tidaktepat',
+        r'\bkurang informatif\b': 'tidakinformatif',
+        r'\bkurang terorganisir\b': 'asal',
+        r'\bkurang detail\b': 'tidakdetail',
+        r'\bkurang memenuhi harapan\b': 'kecewa',
+
+        # Slang dan typo
+        r'\bg layak\b': 'tidaklayak',
+        r'\bgk layak\b': 'tidaklayak',
+        r'\bgak layak\b': 'tidaklayak',
+        r'\btdk layak\b': 'tidaklayak',
+        r'\bkurg layak\b': 'tidaklayak',
+        r'\bkrg layak\b': 'tidaklayak',
+        r'\btdk bersih\b': 'kotor',
+        r'\bg bersih\b': 'kotor',
+        r'\bga bersih\b': 'kotor',
+        r'\bgk bersih\b': 'kotor',
+        r'\bgak bersih\b': 'kotor',
+        r'\btdk bersih\b': 'kotor',
+        r'\bkurg bersih\b': 'kotor',
+        r'\bkurg bagus\b': 'kotor',
+        r'\bkrg bagus\b': 'kotor',
+        r'\bg bagus\b': 'kotor',
+        r'\bga bagus\b': 'kotor',
+        r'\bgak bagus\b': 'kotor',
+        r'\bgk bagus\b': 'kotor',
+        r'\btdk bagus\b': 'kotor',
+        r'\btdk lengkap\b': 'tidaklengkap',
+        r'\btdk lgkp\b': 'tidaklengkap',
+        r'\btidak lgkp\b': 'tidaklengkap',
+        r'\bg lengkap\b': 'tidaklengkap',
+        r'\bga lengkap\b': 'tidaklengkap',
+        r'\bgak lengkap\b': 'tidaklengkap',
+        r'\bgk lengkap\b': 'tidaklengkap',
+        r'\bkurg lengkap\b': 'tidaklengkap',
+        r'\bg sesuai\b': 'gasesuai',
+        r'\bga sesuai\b': 'gasesuai',
+        r'\bgak sesuai\b': 'gasesuai',
+        r'\bgk sesuai\b': 'gasesuai',
+        r'\btdk sesuai\b': 'gasesuai',
+        r'\bkurg sesuai\b': 'gasesuai',
+        r'\btdk cepat\b': 'lambat',
+        r'\bga cepat\b': 'lambat',
+        r'\bgak cepat\b': 'lambat',
+        r'\bgk cepat\b': 'lambat',
+        r'\bkurg cepat\b': 'lambat',
+        r'\btdk nyaman\b': 'tidaknyaman',
+        r'\bgk nyaman\b': 'tidaknyaman',
+        r'\bgak nyaman\b': 'tidaknyaman',
+        r'\bg nyaman\b': 'tidaknyaman',
+        r'\bgkurg nyaman\b': 'tidaknyaman',
+        r'\btdk ramah\b': 'tidakramah',
+        r'\bga ramah\b': 'tidakramah',
+        r'\bgak ramah\b': 'tidakramah',
+        r'\bgk ramah\b': 'tidakramah',
+        r'\bkrg ramah\b': 'tidakramah',
+        r'\bkurg ramah\b': 'tidakramah',
+        r'\bg enak\b': 'tidakenak',
+        r'\bga enak\b': 'tidakenak',
+        r'\bgk enak\b': 'tidakenak',
+        r'\bgak enak\b': 'tidakenak',
+        r'\btdk enak\b': 'tidakenak',
+        r'\bkurg enak\b': 'tidakenak',
+        r'\bkrg enak\b': 'tidakenak',
+        r'\bg aman\b': 'tidakaman',
+        r'\bga aman\b': 'tidakaman',
+        r'\bgak aman\b': 'tidakaman',
+        r'\bgk aman\b': 'tidakaman',
+        r'\btdk aman\b': 'tidakaman',
+        r'\bkurg aman\b': 'tidakaman',
+        r'\bkrg aman\b': 'tidakaman',
+        r'\bg sopan\b': 'tidaksopan',
+        r'\bga sopan\b': 'tidaksopan',
+        r'\bgak sopan\b': 'tidaksopan',
+        r'\bgk sopan\b': 'tidaksopan',
+        r'\btdk sopan\b': 'tidaksopan',
+        r'\bkurg sopan\b': 'tidaksopan',
+        r'\bkrg sopan\b': 'tidaksopan',
+        r'\bg stabil\b': 'tidakstabil',
+        r'\bga stabil\b': 'tidakstabil',
+        r'\bgak stabil\b': 'tidakstabil',
+        r'\bgk stabil\b': 'tidakstabil',
+        r'\btdk stabil\b': 'tidakstabil',
+        r'\bkurg stabil\b': 'tidakstabil',
+        r'\bkrg stabil\b': 'tidakstabil',
+        r'\bg rapi\b': 'berantakan',
+        r'\bga rapi\b': 'berantakan',
+        r'\bgak rapi\b': 'berantakan',
+        r'\bgk rapi\b': 'berantakan',
+        r'\btdk rapi\b': 'berantakan',
+        r'\bkurg rapi\b': 'berantakan',
+        r'\bkrg rapi\b': 'berantakan',
+        r'\bg profesional\b': 'tidakprofesional',
+        r'\bga profesional\b': 'tidakprofesional',
+        r'\bgak profesional\b': 'tidakprofesional',
+        r'\bgk profesional\b': 'tidakprofesional',
+        r'\btdk profesional\b': 'tidakprofesional',
+        r'\bkurg profesional\b': 'tidakprofesional',
+        r'\bkrg profesional\b': 'tidakprofesional',
+        r'\bg efisien\b': 'tidakefisien',
+        r'\bga efisien\b': 'tidakefisien',
+        r'\bgak efisien\b': 'tidakefisien',
+        r'\bgk efisien\b': 'tidakefisien',
+        r'\btdk efisien\b': 'tidakefisien',
+        r'\bkurg efisien\b': 'tidakefisien',
+        r'\bkrg efisien\b': 'tidakefisien',
+        r'\btdk konsisten\b': 'tidakkonsisten',
+        r'\bgak konsisten\b': 'tidakkonsisten',
+        r'\bgk konsisten\b': 'tidakkonsisten',
+        r'\bg konsisten\b': 'tidakkonsisten',
+        r'\btdk matang\b': 'tidakmatang',
+        r'\bg matang\b': 'tidakmatang',
+        r'\bgak matang\b': 'tidakmatang',
+        r'\bgk matang\b': 'tidakmatang',
+        r'\bga matang\b': 'tidakmatang',
+        r'\bkurg matang\b': 'tidakmatang',
+        r'\bkrg matang\b': 'tidakmatang',
+        r'\btdk membantu\b': 'tidakmembantu',
+        r'\bg membantu\b': 'tidakmembantu',
+        r'\bga membantu\b': 'tidakmembantu',
+        r'\bgak membantu\b': 'tidakmembantu',
+        r'\bgk membantu\b': 'tidakmembantu',
+        r'\bkurg membantu\b': 'tidakmembantu',
+        r'\bkrg membantu\b': 'tidakmembantu',
+        r'\btdk wajar\b': 'aneh',
+        r'\bg wajar\b': 'aneh',
+        r'\bga wajar\b': 'aneh',
+        r'\bgak wajar\b': 'aneh',
+        r'\bgk wajar\b': 'aneh',
+        r'\btdk wajar\b': 'aneh',
+        r'\bkrg wajar\b': 'aneh',
+        r'\bkurg wajar\b': 'aneh',
+        r'\btdk peduli\b': 'cuek',
+        r'\bg peduli\b': 'cuek',
+        r'\bga peduli\b': 'cuek',
+        r'\bgak peduli\b': 'cuek',
+        r'\bgk peduli\b': 'cuek',
+        r'\btdk peduli\b': 'cuek',
+        r'\bkurg peduli\b': 'cuek',
+        r'\bkrg peduli\b': 'cuek',
+        r'\btdk terawat\b': 'tidakterawat',
+        r'\bg terawat\b': 'tidakterawat',
+        r'\bgak terawat\b': 'tidakterawat',
+        r'\bgk terawat\b': 'tidakterawat',
+        r'\bkurg terawat\b': 'tidakterawat',
+        r'\bkrg terawat\b': 'tidakterawat',
+        r'\bkrg jujur\b': 'tidakjujur',
+        r'\bga jujur\b': 'tidakjujur',
+        r'\bgak jujur\b': 'tidakjujur',
+        r'\bgk jujur\b': 'tidakjujur',
+        r'\bg jujur\b': 'tidakjujur',
+        r'\btdk jujur\b': 'tidakjujur',
+        r'\bg ada\b': 'tidakada',
+        r'\bga ada\b': 'tidakada',
+        r'\bgak ada\b': 'tidakada',
+        r'\bgk ada\b': 'tidakada',
+        r'\btdk ada\b': 'tidakada',
+        r'\bg bisa\b': 'tidakbisa',
+        r'\bga bisa\b': 'tidakbisa',
+        r'\bgk bisa\b': 'tidakbisa',
+        r'\bgabisa\b': 'tidakbisa',
+        r'\btdkbisa\b': 'tidakbisa',
+
+        # Frase tambahan sesuai konteks ulasan
+        r'\btidak dilayani\b': 'cuek',
+        r'\btdk dilayani\b': 'cuek',
+        r'\bkurang perhatian\b': 'cuek',
+        r'\btdk sesuai\b': 'kecewa',
+        r'\btidak memenuhi harapan\b': 'kecewa',
+        r'\btdk memenuhi harapan\b': 'kecewa',
+        r'\bg memenuhi harapan\b': 'kecewa',
+        r'\btidak sesuai ekspektasi\b': 'kecewa',
+        r'\btidak sesuai\b': 'kecewa',
+        r'\bkurang terorganisir\b': 'asal',
+        r'\bkrg terorganisir\b': 'asal',
+        r'\bkrg terorganisir\b': 'asal',
+        r'\bkurang tanggung jawab\b': 'tidakbertanggungjawab',
+         r'\bkrg bertanggung jawab\b': 'tidakbertanggungjawab',
+        r'\bkurang detail\b': 'tidakdetail',
+        r'\bkrg detail\b': 'tidakdetail',
+        r'\bkrg wangi\b': 'bau',
+        r'\btdk wangi\b': 'bau',
+        r'\bkrg tepat\b': 'tidaktepat',
+        r'\bkrg informatif\b': 'tidakinformatif',
+        r'\bkrg detail\b': 'tidakdetail'
+    }
+    for pattern, replacement in negation_patterns.items():
+        text = re.sub(pattern, replacement, text)
     return text
 
 # Fungsi Preprocessing
@@ -54,7 +287,7 @@ def preprocess_text(text):
     text = clean_text(text)
     
     # Normalisasi
-    text = normalize_text(text)
+    text = normalize_negation(text)
     
     # Stopword Removal
     text = stopword_remover.remove(text)
@@ -204,9 +437,8 @@ def main():
                     # Tampilkan informasi jumlah data yang diproses
                     st.info(f"Total data setelah menghapus ulasan kosong: {len(df)} baris")
     
-                  
                     # Visualisasi Pie Chart
-                    st.subheader("Visualisasi Aspek Dan Sentimen")
+                    st.subheader("Visualisasi Sentimen per Aspek")
                     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
                     aspek_list = ["Fasilitas", "Pelayanan", "Masakan"]
                     colors = ["#66b3ff", "#ff9999"]
@@ -253,5 +485,4 @@ def main():
     """)
 
 if __name__ == "__main__":
-
     main()
