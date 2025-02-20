840410c40a85aa2b8abe938bab1f064509f3976b
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2025-02-20 22:41:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def normalize_negation(text):
         r'\btidak bisa\b': 'tidakbisa',
         r'\btidak rapi\b': 'tidakrapi',
         r'\btidak jelek\b': 'bagus',
-        r'\btidak bersih\b': 'jelek',
+        r'\btidak bersih\b': 'jorok',
 
         # Kata negasi diawali dengan "kurang"
         r'\bkurang bersih\b': 'kotor',
